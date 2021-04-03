a5131e628deb4358be990d8292e474f8e8cdabe9
dima12780 <dima.shagaliev@yandex.ru>
2021-04-03 15:57:34 +0500

    FriendsComponent update

--- front/src/app/components/friends/friends.component.ts
+++ front/src/app/components/friends/friends.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { Users } from '../../mocks/mock-users';
 import { user } from '../../models/user';
 
@@ -7,31 +7,65 @@ import { user } from '../../models/user';
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.css']
 })
+
 export class FriendsComponent implements OnInit {
 
+  @ViewChild('myInputOne') myInputOne!: ElementRef;
+  @ViewChild('myInputTwo') myInputTwo!: ElementRef;
+  @ViewChild('delFriend') delFriend!: ElementRef;
+
   Users = Users;
-  public search: any;
-  public friend: any;
-  public friends: user[] = [];
+
+  public search: string = "";
+  public user!: user;
+  public selectedFriend!: user;
+  public searchFriend: string = "";
+  public allFriends: user[] = [];
+  public searchFrends: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    let user = JSON.parse(localStorage.user);
-    let MyFriends = user.friends;
+    this.user = JSON.parse(localStorage.user);
+    this.selectedFriend = this.user;
+    let MyFriends = this.user.friends;
     if (MyFriends) {
       for (let index = 0; index < MyFriends.length; index++) {
-        this.friends.push(this.searchFrends(this.Users, MyFriends[index]));
+        this.allFriends.push(this.searchAllFrends(this.Users, MyFriends[index]));
+        this.searchFrends.push(this.searchAllFrends(this.Users, MyFriends[index]));
       }
     }
   }
 
-  searchFrends(Users: any[], id: number) {
+  cleaningInput(bool: boolean){
+    bool ? this.myInputOne.nativeElement.value = "" : this.myInputTwo.nativeElement.value = "";
+  }
+
+  searchName(input: any[], search: string) {
+    if (search === "") {
+      this.searchFrends = this.allFriends;
+    }else{
+      search = search.toLowerCase();
+      var output = [];
+      for(var i=0; i<input.length; i++) {
+        if(input[i].Name.toLowerCase().indexOf(search) !== -1 && input[i].id !== this.user.id) {
+          output.push(input[i]);
+        }
+      }
+      this.searchFrends = output;
+    }
+  }
+
+  searchAllFrends(Users: any[], id: number) {
     for (let index = 0; index < Users.length; index++) {
       if (Users[index].id === id){
         return Users[index];
       } 
     }
+  }
+
+  onSelect(friend: user): void {
+    this.selectedFriend = friend;
   }
 
 }
--- front/src/app/mocks/mock-users.ts
+++ front/src/app/mocks/mock-users.ts
@@ -23,6 +23,62 @@ export const Users: user[] = [
         Email: "@org.su",
         Password: "456",
         scores: [Scores[2]]
+    },{
+        id: 4,
+        Name: 'Wow',
+        Email: "",
+        Password: "1",
+        scores: []
+    },{
+        id: 5,
+        Name: 'Anna Maria',
+        Email: "",
+        Password: "1",
+        scores: []
+    },{
+        id: 6,
+        Name: 'Tony Stark',
+        Email: "",
+        Password: "111",
+        scores: [Scores[6]],
+        friends: [5, 7, 3],
+        history: Operation
+    },{
+        id: 7,
+        Name: 'Vasya Pypcin',
+        Email: "",
+        Password: "0",
+        scores: []
+    },{
+        id: 8,
+        Name: 'Lida Pypcina',
+        Email: "",
+        Password: "0",
+        scores: []
+    },{
+        id: 9,
+        Name: 'asd',
+        Email: "",
+        Password: "0",
+        scores: []
+    },{
+        id: 10,
+        Name: 'abcde',
+        Email: "",
+        Password: "0",
+        scores: []
+    },{
+        id: 11,
+        Name: 'asd_1',
+        Email: "",
+        Password: "0",
+        scores: []
+    },{
+        id: 12,
+        Name: 'waw"WWW"',
+        Email: "",
+        Password: "0",
+        scores: []
     },
 
 ]
